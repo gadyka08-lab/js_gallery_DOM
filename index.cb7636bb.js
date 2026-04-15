e60d5608a678e96ac70115733a64b6331066d9fa
Natalia Lunkevych <gadyka08@gmail.com>
2026-04-15 20:19:36 +0300

    make build

---
+++ index.cb7636bb.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){var t=e.target.closest("a");t&&(e.preventDefault(),document.getElementById("largeImg").src=t.href)});
+//# sourceMappingURL=index.cb7636bb.js.map
